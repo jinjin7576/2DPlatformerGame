e8db5ccdd831f323135bbb924b3eff8855b4db13
jinjin7576 <jinjin7576@daum.net>
2025-08-05 13:37:11 +0900

    2
    2

--- 2DPlatformerGame/Assets/Scripts/Platform/PlatformMoving.cs
+++ 2DPlatformerGame/Assets/Scripts/Platform/PlatformMoving.cs
@@ -5,56 +5,11 @@ using UnityEngine;
 
 public class PlatformMoving : MonoBehaviour
 {
-
     [SerializeField]
     Transform target;
+    #region "region"
 
-    [SerializeField]
-    Transform[] wayPoints;
-
-    [SerializeField]
-    float waitTime;
-
-    [SerializeField]
-    float timeOffset;
-
-    private int wayPointCount;
-    private int currentIndex = 0;
-
-    private void Awake()
-    {
-        target.position = wayPoints[currentIndex].position;
-        wayPointCount = wayPoints.Length;
-
-        currentIndex++;
-
-        StartCoroutine(nameof(Process));
-    }
-
-    private IEnumerator Process()
-    {
-        while (true)
-        {
-            yield return StartCoroutine(MoveAToB(target.position, wayPoints[currentIndex].position));
-
-            yield return new WaitForSeconds(waitTime);
-            if (currentIndex < wayPointCount - 1) currentIndex++;
-            else currentIndex = 0;
-        }
-    }
-
-    private IEnumerator MoveAToB(Vector3 a, Vector3 b)
-    {
-        float percent = 0;
-        float moveTime = Vector3.Distance(a, b) * timeOffset;
-        while (percent < 1)
-        {
-            percent += Time.deltaTime / moveTime;
-            target.position = Vector3.Lerp(a, b, percent);
-            yield return null;  
-        }
-    }
-
+    #endregion
     private void OnCollisionEnter2D(Collision2D collision)
     {
         collision.transform.SetParent(target.transform);

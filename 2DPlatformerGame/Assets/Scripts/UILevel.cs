c5fa0ddd28a7de713d42fee03044bae470df7ca1
jinjin7576 <jinjin7576@daum.net>
2025-08-07 13:53:39 +0900

    today end
    today end

--- 2DPlatformerGame/Assets/Scripts/UILevel.cs
+++ 2DPlatformerGame/Assets/Scripts/UILevel.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class UILevel : MonoBehaviour, IPointerClickHandler
 {
@@ -24,6 +25,42 @@ public class UILevel : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        
+        if (isUnlock == true)
+        {
+            imageFadeScreen.gameObject.SetActive(true);
+            StartCoroutine(FadeEffect.Fade(imageFadeScreen, 0, 1, 1 ,AfterFadeEffect));
+        }
     }
+
+    private void AfterFadeEffect()
+    {
+        PlayerPrefs.SetInt(Constants.CurrentLevel, level);
+        Utils.LoadScene(SceneName.Game);
+    }
+
+    public void SetLevel(int level, bool isUnlock, bool[] stars, Image imageFadeScreen)
+    {
+        this.level = level;
+        this.isUnlock = isUnlock;
+        this.imageFadeScreen = imageFadeScreen;
+
+        if (isUnlock == true)
+        {
+            textLevel.enabled = true;
+            textLevel.text = level.ToString();
+        }
+        else
+        {
+            imageLevelIcon.sprite = spriteLevelLock;
+            textLevel.enabled = false;
+
+            starBackgroundObject.SetActive(false);
+        }
+
+        for( int i =0; i < starObjects.Length; ++i)
+        {
+            starObjects[i].SetActive(stars[i]);
+        }
+    }
+
 }
